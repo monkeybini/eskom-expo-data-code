d67db565755b15263e614c675508ac41afa5bf72
monkeybini <binyameenie2@gmail.com>
2024-04-26 00:45:46 +0200

    update of function and data

--- data.py
+++ data.py
@@ -2,6 +2,11 @@ import math
 
 
 
+def is_prime(n):
+	for i in range(2,n):
+		if n%i==0:
+			return False
+	return True
 def calc(n,k):
 	prev = k
 	nxt = 0
@@ -9,13 +14,15 @@ def calc(n,k):
 		diff = math.gcd(i,prev)
 		nxt = prev + diff
 		
-		if nxt>3*k/2:
-			print(i,nxt,3*k/2+1,k,k/2+1)
+		if nxt>3*k/2 and nxt/i == 3 and diff>1:
+			if is_prime(diff)==False:
+				print("counter example found")
+			print(i,nxt, diff,k)
 			break
 	
 		prev = nxt
 
-for j in range(4,1000,2):
-	calc(j,j)
+for j in range(4,10000,2):
+	calc(2*j,j)
 
 
