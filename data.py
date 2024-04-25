36375c02d0163ab0c366f757cc1d652d2d867dab
monkeybini <binyameenie2@gmail.com>
2024-04-26 00:36:30 +0200

    main work put in

--- data.py
+++ data.py
@@ -1,1 +1,21 @@
-print("damn")
+import math
+
+
+
+def calc(n,k):
+	prev = k
+	nxt = 0
+	for i in range(2,n+1):
+		diff = math.gcd(i,prev)
+		nxt = prev + diff
+		
+		if nxt>3*k/2:
+			print(i,nxt,3*k/2+1,k,k/2+1)
+			break
+	
+		prev = nxt
+
+for j in range(4,1000,2):
+	calc(j,j)
+
+
